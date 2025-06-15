c99eea65e8d89e45f8ffacec16f6b0be5730b8cd
WhoseLow <whose.low@protonmail.com>
2025-06-15 18:37:50 -0400

    remove files

---
+++ .angular/cache/19.2.15/ginnms.github.io/vite/deps/@angular_cdk_bidi.js
@@ -0,0 +1,15 @@
+import {
+  BidiModule,
+  DIR_DOCUMENT,
+  Dir,
+  Directionality
+} from "./chunk-U66QJ4I7.js";
+import "./chunk-AVGZ6SDF.js";
+import "./chunk-KPMLMQWB.js";
+import "./chunk-VQCXTS5Z.js";
+export {
+  BidiModule,
+  DIR_DOCUMENT,
+  Dir,
+  Directionality
+};
---
+++ .angular/cache/19.2.15/ginnms.github.io/vite/deps/@angular_cdk_scrolling.js
@@ -0,0 +1,49 @@
+import {
+  CdkFixedSizeVirtualScroll,
+  CdkScrollable,
+  CdkScrollableModule,
+  CdkVirtualForOf,
+  CdkVirtualScrollViewport,
+  CdkVirtualScrollable,
+  CdkVirtualScrollableElement,
+  CdkVirtualScrollableWindow,
+  DEFAULT_RESIZE_TIME,
+  DEFAULT_SCROLL_TIME,
+  FixedSizeVirtualScrollStrategy,
+  ScrollDispatcher,
+  ScrollingModule,
+  VIRTUAL_SCROLLABLE,
+  VIRTUAL_SCROLL_STRATEGY,
+  ViewportRuler,
+  _fixedSizeVirtualScrollStrategyFactory
+} from "./chunk-MXINXC7C.js";
+import "./chunk-ZLKYONTJ.js";
+import "./chunk-2O4WY5GE.js";
+import "./chunk-ZM4VGGN3.js";
+import {
+  Dir
+} from "./chunk-U66QJ4I7.js";
+import "./chunk-SRDYTCDW.js";
+import "./chunk-AVGZ6SDF.js";
+import "./chunk-KPMLMQWB.js";
+import "./chunk-VQCXTS5Z.js";
+export {
+  CdkFixedSizeVirtualScroll,
+  CdkScrollable,
+  CdkScrollableModule,
+  CdkVirtualForOf,
+  CdkVirtualScrollViewport,
+  CdkVirtualScrollable,
+  CdkVirtualScrollableElement,
+  CdkVirtualScrollableWindow,
+  DEFAULT_RESIZE_TIME,
+  DEFAULT_SCROLL_TIME,
+  FixedSizeVirtualScrollStrategy,
+  ScrollDispatcher,
+  ScrollingModule,
+  VIRTUAL_SCROLLABLE,
+  VIRTUAL_SCROLL_STRATEGY,
+  ViewportRuler,
+  _fixedSizeVirtualScrollStrategyFactory,
+  Dir as ɵɵDir
+};
